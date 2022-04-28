166ebb42ceba68f358ffb35441d6a6bbd9d28cfb
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-04-28 18:51:29 +0200

    Chapter 3 is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventsListCompononent } from './events/events-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
+import { NavBarComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListCompononent,
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
