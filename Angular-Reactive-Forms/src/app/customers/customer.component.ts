b6334cc6715a968ea94cd794ee9975d6cacffa75
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-05-03 08:40:55 +0200

    Todo fix bug with control paths

--- Angular-Reactive-Forms/src/app/customers/customer.component.ts
+++ Angular-Reactive-Forms/src/app/customers/customer.component.ts
@@ -1,7 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 import { Customer } from './customer';
+
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
+  const emailControl = c.get('email');
+  const confirmControl = c.get('confirmEmail');
+
+  if (emailControl.pristine || confirmControl.pristine) {
+    return null;
+  }
+
+  if (emailControl.value === confirmControl.value) {
+    return null;
+  }
+  return { match: true };
+}
+
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)) {
+      return { range: true };
+    }
+    return null;
+  };
+}
 
 @Component({
   selector: 'app-customer',
@@ -11,14 +34,21 @@ import { Customer } from './customer';
 export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
   customer = new Customer();
+  emailMessage: string;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.customerForm = this.fb.group({
-      firstName: '',
-      lastName: '',
-      email: '',
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        confirmEmail: ['', Validators.required],
+      }),
+      phone: '',
+      notification: 'email',
+      rating: [null, ratingRange(1,5)],
       sendCatalog: true
     });
   }
@@ -30,6 +60,17 @@ export class CustomerComponent implements OnInit {
       sendCatalog: false
     })
   }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
+  }
+
   save(): void {
 
   }
