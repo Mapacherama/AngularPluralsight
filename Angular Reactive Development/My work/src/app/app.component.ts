1fb8086ce05cb9f77fd072ac5ee8eba93598c5b3
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-05-07 07:04:17 +0200

    Just checking.

--- Angular Reactive Development/My work/src/app/app.component.ts
+++ Angular Reactive Development/My work/src/app/app.component.ts
@@ -12,11 +12,18 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
   of(2,4,6,8).subscribe(item => console.log(item));
+
   from([20,15,10,5]).subscribe({
     next: (item) => console.log(`resulting item... ${item}`),
     error: (err) => console.error(`error occurred ${err}`),
     complete: () => console.log('complete'),
 
+  });
+
+  of('Apple1','Apple2','Apple3','Apple4').subscribe({
+    next: (item) => console.log(`Apple comming through! ... ... ${item}`),
+    error: (err) => console.error(`error occurred ${err}`),
+    complete: () => console.log('Apples devoured'),
   });
   }
 }
