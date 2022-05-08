a80dbe81bb5b0071d9155fcd3c4e944fefa71f92
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-05-08 06:29:00 +0200

    Module 5 of reactive development is done.

--- Angular Reactive Development/My work/src/app/app.component.ts
+++ Angular Reactive Development/My work/src/app/app.component.ts
@@ -1,29 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { from, of } from 'rxjs';
-
+import { from, of, map, tap, take } from 'rxjs';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   pageTitle = 'Acme Product Management';
-
-  ngOnInit() {
-  of(2,4,6,8).subscribe(item => console.log(item));
-
-  from([20,15,10,5]).subscribe({
-    next: (item) => console.log(`resulting item... ${item}`),
-    error: (err) => console.error(`error occurred ${err}`),
-    complete: () => console.log('complete'),
-
-  });
-
-  of('Apple1','Apple2','Apple3','Apple4').subscribe({
-    next: (item) => console.log(`Apple comming through! ... ... ${item}`),
-    error: (err) => console.error(`error occurred ${err}`),
-    complete: () => console.log('Apples devoured'),
-  });
-  }
 }
