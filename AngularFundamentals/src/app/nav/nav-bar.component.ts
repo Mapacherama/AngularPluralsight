d87a9560bcac8efc4a1d47a52f8136f7f11a52bc
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-06-17 06:49:59 +0200

    Small little save

--- AngularFundamentals/src/app/nav/nav-bar.component.ts
+++ AngularFundamentals/src/app/nav/nav-bar.component.ts
@@ -1,18 +1,38 @@
-import { Component } from '@angular/core'
-import { AuthService } from '../user/auth.service'
+import { Component } from "@angular/core";
+import { AuthService } from "../user/auth.service";
+import { ISession } from "../events/shared/event.model";
+import { EventService } from "../events/shared/event.service";
 
 @Component({
-  selector: 'nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styles: [`
-    .nav.navbar-nav {font-size: 15px;}
-    #searchForm {margin-right: 100px;}
-    @media (max-width: 1200px) {#searchForm {display:none}}
-    li > a.active { color: #F97924; }
-  `]
+  selector: "nav-bar",
+  templateUrl: "./nav-bar.component.html",
+  styles: [
+    `
+      .nav.navbar-nav {
+        font-size: 15px;
+      }
+      #searchForm {
+        margin-right: 100px;
+      }
+      @media (max-width: 1200px) {
+        #searchForm {
+          display: none;
+        }
+      }
+      li > a.active {
+        color: #f97924;
+      }
+    `,
+  ],
 })
 export class NavBarComponent {
-  constructor(public auth: AuthService) {
-    
+  searchTerm: string = "";
+  foundSessions: ISession[];
+  constructor(public auth: AuthService, private eventService: EventService) {}
+
+  searchSessions(searchTerm) {
+    this.eventService.searchSessions(searchTerm).subscribe((sessions) => {
+      this.foundSessions = sessions;
+    });
   }
 }
