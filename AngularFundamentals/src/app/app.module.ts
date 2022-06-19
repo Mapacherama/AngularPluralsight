9990f034bb24de30137b27e05d6d37aeff35b869
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-06-19 08:50:36 +0200

    Module 13 - Angular Fundamentals is finished. Fixed a routing bug.

--- AngularFundamentals/src/app/app.module.ts
+++ AngularFundamentals/src/app/app.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { RouterModule } from '@angular/router'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import {
   EventsListComponent,
@@ -9,28 +10,39 @@ import {
   EventService,
   EventDetailsComponent,
   CreateEventComponent,
-  EventRouteActivator,
+  EventResolver,
   EventListResolver,
   CreateSessionComponent,
   SessionListComponent,
-  DurationPipe
-} from './events/index'
-import { EventsAppComponent } from './events-app.component'
-import { NavBarComponent } from './nav/nav-bar.component'
-import { JQ_TOKEN, TOASTR_TOKEN, Toastr, CollapsibleWellComponent, SimpleModalComponent, ModalTriggerDirective } from './common/index'
-import { appRoutes } from './routes'
-import { Error404Component } from './errors/404.component'
-import { AuthService } from './user/auth.service'
+  UpvoteComponent,
+  VoterService,
+  LocationValidator,
+  DurationPipe,
+} from "./events/index";
+import { EventsAppComponent } from "./events-app.component";
+import { NavBarComponent } from "./nav/nav-bar.component";
+import {
+  JQ_TOKEN,
+  TOASTR_TOKEN,
+  Toastr,
+  CollapsibleWellComponent,
+  SimpleModalComponent,
+  ModalTriggerDirective,
+} from "./common/index";
+import { appRoutes } from "./routes";
+import { Error404Component } from "./errors/404.component";
+import { AuthService } from "./user/auth.service";
 
-let toastr:Toastr = window['toastr'];
-let jQuery = window['$'];
+let toastr: Toastr = window["toastr"];
+let jQuery = window["$"];
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
   ],
   declarations: [
     EventsAppComponent,
@@ -45,27 +57,31 @@ let jQuery = window['$'];
     CollapsibleWellComponent,
     ModalTriggerDirective,
     SimpleModalComponent,
-
-    DurationPipe
+    UpvoteComponent,
+    LocationValidator,
+    DurationPipe,
   ],
   providers: [
-    EventService, 
-    { provide: TOASTR_TOKEN, useValue: toastr}, 
-    { provide: JQ_TOKEN, useValue: jQuery}, 
-    EventRouteActivator,
+    EventService,
+    { provide: TOASTR_TOKEN, useValue: toastr },
+    { provide: JQ_TOKEN, useValue: jQuery },
+    EventResolver,
     EventListResolver,
+    VoterService,
     AuthService,
-    { 
-      provide: 'canDeactivateCreateEvent', 
-      useValue: checkDirtyState 
-    }
+    {
+      provide: "canDeactivateCreateEvent",
+      useValue: checkDirtyState,
+    },
   ],
-  bootstrap: [EventsAppComponent]
+  bootstrap: [EventsAppComponent],
 })
 export class AppModule {}
 
-export function checkDirtyState(component:CreateEventComponent) {
+export function checkDirtyState(component: CreateEventComponent) {
   if (component.isDirty)
-    return window.confirm('You have not saved this event, do you really want to cancel?')
-  return true
+    return window.confirm(
+      "You have not saved this event, do you really want to cancel?"
+    );
+  return true;
 }
