dcbfae22275a07f3cbb954fd1975467a158b3871
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-06-19 13:47:47 +0200

    Module 14 + 15 done today. That is it!

--- AngularFundamentals/src/app/events-app.component.ts
+++ AngularFundamentals/src/app/events-app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core'
+import { Component } from "@angular/core";
+import { AuthService } from "./user/auth.service";
 
 @Component({
-  selector: 'events-app',
+  selector: "events-app",
   template: `
     <nav-bar></nav-bar>
     <router-outlet></router-outlet>
-  `
+  `,
 })
 export class EventsAppComponent {
-  title = 'app';
+  constructor(private auth: AuthService) {}
+  ngOnInit() {
+    this.auth.checkAuthenticationStatus();
+  }
 }
--- AngularFundamentals/src/app/events/shared/event.service.ts
+++ AngularFundamentals/src/app/events/shared/event.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { EventEmitter, Injectable } from "@angular/core";
 import { Subject, Observable, of } from "rxjs";
 import { catchError } from "rxjs/operators";
@@ -21,36 +21,18 @@ export class EventService {
   }
 
   saveEvent(event) {
-    event.id = 999;
-    event.session = [];
-    EVENTS.push(event);
+    let options = {
+      headers: new HttpHeaders({ "Content-Type": "application/json" }),
+    };
+    return this.http
+      .post<IEvent>("/api/events", event, options)
+      .pipe(catchError(this.handleError<IEvent>("saveEvent")));
   }
 
-  updateEvent(event) {
-    let index = EVENTS.findIndex((x) => (x.id = event.id));
-    EVENTS[index] = event;
-  }
-
-  searchSessions(searchTerm: string) {
-    var term = searchTerm.toLocaleLowerCase();
-    var results: ISession[] = [];
-
-    EVENTS.forEach((event) => {
-      var matchingSessions = event.sessions.filter(
-        (session) => session.name.toLocaleLowerCase().indexOf(term) > -1
-      );
-      matchingSessions = matchingSessions.map((session: any) => {
-        session.eventId = event.id;
-        return session;
-      });
-      results = results.concat(matchingSessions);
-    });
-
-    var emitter = new EventEmitter(true);
-    setTimeout(() => {
-      emitter.emit(results);
-    }, 100);
-    return emitter;
+  searchSessions(searchTerm: string): Observable<ISession[]> {
+    return this.http
+      .get<ISession[]>("/api/sessions/search?search=" + searchTerm)
+      .pipe(catchError(this.handleError<ISession[]>("searchSession", [])));
   }
 
   private handleError<T>(operation = "operation", result?: T) {
