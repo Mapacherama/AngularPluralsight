cf386a6a925405a6449e3078b30571d42fd09cb9
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-06-21 05:44:10 +0200

    Working through Module 16 of Angular Fundamentals

---
+++ AngularFundamentals/src/app/events/event-details/session-list.component.integrated.spec.ts
@@ -0,0 +1,55 @@
+import { DebugElement } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { SessionListComponent } from "./session-list.component";
+import { AuthService } from "../../user/auth.service";
+import { VoterService } from "./voter.service";
+import { DurationPipe } from "../shared/duration.pipe";
+
+describe("SessionListComponent", () => {
+  let mockAuthService,
+    mockVoterService,
+    fixture: ComponentFixture<SessionListComponent>,
+    component: SessionListComponent,
+    element: HTMLElement,
+    debugEl: DebugElement;
+
+  beforeEach(() => {
+    mockAuthService = {
+      isAuthenticated: () => true,
+      currentUser: { userName: "Joe" },
+    };
+    mockVoterService = { userHasVoted: () => true };
+    TestBed.configureTestingModule({
+      declarations: [SessionListComponent, DurationPipe],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: VoterService, useValue: mockVoterService },
+      ],
+    });
+    fixture = TestBed.createComponent(SessionListComponent);
+    component = fixture.componentInstance;
+    debugEl = fixture.debugElement;
+    element = fixture.nativeElement;
+  });
+
+  describe("initial display", () => {
+    it("should have the correct title", () => {
+      component.sessions = [
+        {
+          name: "Session 1",
+          id: 3,
+          presenter: "Joe",
+          duration: 1,
+          level: "beginner",
+          abstract: "abstract",
+          voters: ["john", "bob"],
+        },
+      ];
+      component.filterBy = "all";
+      component.sortBy = "name";
+      component.eventId = 4;
+      component.ngOnChanges();
+      fixture.detectChanges();
+    });
+  });
+});
